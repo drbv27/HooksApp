aedd4a3d31fbd2cd148b53b88a6141f496e2309e
Diego <drbv27@gmail.com>
2021-10-04 16:44:44 -0500

    memo and useMemo

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import { MemoHook } from "./components/06-memos/MemoHook";
 
 /* import { FormWithCustomHook } from "./components/02-useEffect/FormWithCustomHook"; */
 // import { CounterApp } from "./components/01-useState/CounterApp";
@@ -9,6 +10,7 @@ import ReactDOM from "react-dom";
 /* import { MultipleCustomHook } from "./components/03-examples/MultipleCustomHook"; */
 /* import { FocusScreen } from "./components/04-useRef/FocusScreen"; */
 /* import { RealExampleRef } from "./components/04-useRef/RealExampleRef"; */
-import { Layout } from "./components/05-useLayoutEffect/LayoutEffect";
+/* import { Layout } from "./components/05-useLayoutEffect/LayoutEffect"; */
+/* import { Memorize } from "./components/06-memos/Memorize"; */
 
-ReactDOM.render(<Layout />, document.getElementById("root"));
+ReactDOM.render(<MemoHook />, document.getElementById("root"));
