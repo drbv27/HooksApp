be118adf6be4a9f00ef3ac6852e44c65080fe8b0
Diego <drbv27@gmail.com>
2021-10-10 19:33:33 -0500

    Homework

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import { CallbackHook } from "./components/06-memos/CallbackHook";
+/* import { CallbackHook } from "./components/06-memos/CallbackHook"; */
+import { Padre } from "./components/07-tarea-memo/Padre";
 /* import { MemoHook } from "./components/06-memos/MemoHook"; */
 
 /* import { FormWithCustomHook } from "./components/02-useEffect/FormWithCustomHook"; */
@@ -14,4 +15,4 @@ import { CallbackHook } from "./components/06-memos/CallbackHook";
 /* import { Layout } from "./components/05-useLayoutEffect/LayoutEffect"; */
 /* import { Memorize } from "./components/06-memos/Memorize"; */
 
-ReactDOM.render(<CallbackHook />, document.getElementById("root"));
+ReactDOM.render(<Padre />, document.getElementById("root"));
