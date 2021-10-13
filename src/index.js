a8b37684006c7988d1a9bf8e09fd3af94e6bb30d
Diego <drbv27@gmail.com>
2021-10-12 20:48:45 -0500

    Todos app with reducer first commit

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
-// import React from "react";
-// import ReactDOM from "react-dom";
+import React from "react";
+import ReactDOM from "react-dom";
+import { TodoApp } from "./components/08-useReducer/TodoApp";
 // /* import { CallbackHook } from "./components/06-memos/CallbackHook"; */
 // import { Padre } from "./components/07-tarea-memo/Padre";
 // /* import { MemoHook } from "./components/06-memos/MemoHook"; */
@@ -14,7 +15,6 @@
 // /* import { RealExampleRef } from "./components/04-useRef/RealExampleRef"; */
 // /* import { Layout } from "./components/05-useLayoutEffect/LayoutEffect"; */
 // /* import { Memorize } from "./components/06-memos/Memorize"; */
+/* import "./components/08-useReducer/intro-reducer"; */
 
-// ReactDOM.render(<Padre />, document.getElementById("root"));
-
-import "./components/08-useReducer/intro-reducer";
+ReactDOM.render(<TodoApp />, document.getElementById("root"));
